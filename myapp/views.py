23e704e12a958f1798a307de107e168d68777823
AlbertoCon <albertocon209@gmail.com>
2024-10-24 23:37:19 -0600

    Carga de imagenes

--- myapp/views.py
+++ myapp/views.py
@@ -1,17 +1,20 @@
-from django.shortcuts import render, redirect
-from .forms import ImagenForm
+# myapp/views.py
 from django.http import JsonResponse
-
-# Create your views here.
-
+from django.shortcuts import render
+from .models import Imagen
 
 def cargar_imagen(request):
     if request.method == 'POST':
-        form = ImagenForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('cargar_imagen')
-    else:
-        form = ImagenForm()
-    return render(request, 'cargar_imagen.html', {'form': form})
+        # Validar si se subió un archivo
+        if 'file' not in request.FILES:
+            return JsonResponse({'error': 'No se subió ningún archivo'}, status=400)
 
+        # Crear una instancia del modelo y guardar la imagen
+        imagen = Imagen.objects.create(
+            nombre=request.FILES['file'].name,  # Usa el nombre del archivo como 'nombre'
+            imagen=request.FILES['file']
+        )
+
+        return JsonResponse({'message': 'Imagen subida con éxito', 'id': imagen.id})
+
+    return render(request, 'cargar_imagen.html')
